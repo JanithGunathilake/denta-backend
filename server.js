cd0a7aab04cda4ffe852288288ad2b7fee523254
shaluka samaranayaka <shaluws95@gmail.com>
2024-10-26 00:42:04 +0530

    add new api

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors'); 
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-app.use(cors()); 
+app.use(cors());
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-app.post('/process-image', upload.single('image'), async (req, res) => {
-    try {
-        const { client } = await import('@gradio/client');
+app.post("/process-image", upload.single("image"), async (req, res) => {
+  try {
+    const { client } = await import("@gradio/client");
 
-  
-        const imageBase64 = req.file.buffer.toString('base64');
-        
-        const appClient = await client(process.env.GRADIO_API_URL);
-        const result = await appClient.predict("/predict", [`data:image/jpeg;base64,${imageBase64}`]);
+    const imageBase64 = req.file.buffer.toString("base64");
 
-        console.log("API Response:", result);
+    const appClient = await client(process.env.GRADIO_API_URL);
+    const result = await appClient.predict("/predict", [
+      `data:image/jpeg;base64,${imageBase64}`,
+    ]);
 
-        const goodTeethImage = result.data[0]; 
-        const badTeethImage = result.data[1]; 
+    console.log("API Response:", result);
 
-       
-        const goodTeethImageUrl = `data:image/jpeg;base64,${goodTeethImage}`;
-        const badTeethImageUrl = `data:image/jpeg;base64,${badTeethImage}`;
+    const goodTeethImage = result.data[0];
+    const badTeethImage = result.data[1];
 
-        res.json({
-            good_teeth_img: goodTeethImageUrl,
-            bad_teeth_img: badTeethImageUrl
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ message: 'Error processing the image', error });
-    }
+    const goodTeethImageUrl = `${goodTeethImage}`;
+    const badTeethImageUrl = `${badTeethImage}`;
+
+    res.json({
+      good_teeth_img: goodTeethImageUrl,
+      bad_teeth_img: badTeethImageUrl,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Error processing the image", error });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
